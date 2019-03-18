2d6ebb128eeb12b7abb45b849539da80f5ebb948
David Korff <dkorff12@gmail.com>
2019-03-18 08:40:28 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,32 +1,4 @@
 class SongsController < ApplicationController
-
-  def new
-    @song = Song.new
-  end
-
-  def create
-    @song = Song.new(song_params)
-    if @song.valid?
-      @song.save
-      redirect_to song_path(@song)
-    else
-      render :new
-    end
-  end
-
-  def edit
-    @song = Song.find(params[:id])
-  end
-
-  def update
-    @song = Song.find(params[:id])
-    if @song.update(song_params)
-      redirect_to(@song)
-    else
-      render :edit
-    end
-  end
-
   def index
     @songs = Song.all
   end
@@ -35,15 +7,43 @@ class SongsController < ApplicationController
     @song = Song.find(params[:id])
   end
 
+  def new
+    @song = Song.new
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def create
+    @song = Song.new(song_params)
+    if @song.save
+      redirect_to @song
+    else
+      render :new
+    end
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    if @song.update(song_params)
+      redirect_to @song
+    else
+      render :edit
+    end
+  end
+
   def destroy
     @song = Song.find(params[:id])
     @song.destroy
-    redirect_to songs_path, notice: "Delete success"
+    redirect_to songs_url
   end
 
   private
 
   def song_params
-    params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
+    params.require(:song).permit(
+      :title, :release_year, :released, :genre, :artist_name
+    )
   end
 end
